47ba7ab07ad8784b6b27646dc80a5716a5479ef9
Thor Whalen <thorwhalen1@gmail.com>
2023-03-08 15:58:16 +0100

    chore: trying to put the importlib_resourcesconditional in setup.cfg

--- config2py/tests/__init__.py
+++ config2py/tests/__init__.py
@@ -1,9 +1,11 @@
 import sys
 
-try:
-    from importlib.resources import files  # ... and any other things you want to get
-except ImportError:
-    from importlib_resources import files  # pip install importlib_resources
+# try:
+#     from importlib.resources import files  # ... and any other things you want to get
+# except (ImportError, ModuleNotFoundError):
+#     from importlib_resources import files  # pip install importlib_resources
+
+from importlib_resources import files
 
 module_path = files(sys.modules[__name__])
 ppath = module_path.joinpath
